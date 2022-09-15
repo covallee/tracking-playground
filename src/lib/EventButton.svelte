772867f9d05d8890618ff37e3548934c7a5c95fe
Constant V <constantvallee@gmail.com>
2022-09-15 19:38:02 -0400

    feat: add cutom event btn

---
+++ src/lib/EventButton.svelte
@@ -0,0 +1,18 @@
+<script>
+
+const handleClick = () => {
+    window.dataLayer.push({
+	'event' : 'login',
+	'eventName' : 'email' 
+});
+}
+
+</script>
+
+<button class="event-btn" on:click={handleClick}>Click Me</button>
+
+<style>
+    .event-btn{
+        padding: 1rem 2rem;
+    }
+</style>
---
+++ src/routes/custom-event/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import EventButton from "$lib/EventButton.svelte";
+
+</script>
+<svelte:head>
+	<title>Custom Event</title>
+	<meta name="description" content="Custom Event Testing Area" />
+</svelte:head>
+
+<div class="content">
+	<h1>Testing Area for Custom Events</h1>
+
+    <button>Click Me</button>
+
+    <EventButton />
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
